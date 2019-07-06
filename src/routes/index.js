cb17f719d7e4467701bd0354fa05fe448ea6cd5e
Wahab Bello <wahaaabello@gmail.com>
2019-07-06 21:57:29 +0100

    feature: POST /auth/signup functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,30 @@
+import bcrypt from 'bcrypt';
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
+import { asyncHelper } from '../helpers';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.status(200)
+router.post('/auth/signup', asyncHelper(async (req, res) => {
+  const {
+    firstName, lastName, email, password,
+  } = req.body;
+  log(req.body);
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const { rows: [{ id, is_admin: isAdmin }] } = await db.query(
+    'INSERT INTO "user"(first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING id, is_admin;',
+    [firstName, lastName, email, hashedPassword],
+  );
+  const token = jwt.sign({ user_id: id }, process.env.JWT_SECRET);
+  res.status(201)
     .json({
       status: 'success',
-      data: {},
+      data: {
+        user_id: id,
+        token,
+        is_admin: isAdmin,
+      },
     });
-});
+}));
 
 export default router;
