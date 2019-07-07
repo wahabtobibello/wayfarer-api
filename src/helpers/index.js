74b2b564acdd44cc3a19edb05a1c12b2f17628e9
Wahab Bello <wahaaabello@gmail.com>
2019-07-07 12:32:01 +0100

    chore: added postgresql integrity violation error codes

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,2 +1,32 @@
 export const asyncHelper = fn => (req, res, next) => Promise.resolve(fn(req, res, next))
   .catch(next);
+export const SQLErrorCodes = {
+  integrity_constraint_violation: {
+    name: 'integrity_constraint_violation',
+    code: '23000',
+  },
+  restrict_violation: {
+    name: 'restrict_violation',
+    code: '23001',
+  },
+  not_null_violation: {
+    name: 'not_null_violation',
+    code: '23502',
+  },
+  foreign_key_violation: {
+    name: 'foreign_key_violation',
+    code: '23503',
+  },
+  unique_violation: {
+    name: 'unique_violation',
+    code: '23505',
+  },
+  check_violation: {
+    name: 'check_violation',
+    code: '23514',
+  },
+  exclusion_violation: {
+    name: 'exclusion_violation',
+    code: '23P01',
+  },
+};
