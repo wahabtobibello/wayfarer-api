bbd3de8f73c013df365eee0321cbe22960216eae
Wahab Bello <wahaaabello@gmail.com>
2019-07-14 21:37:16 +0100

    test: test for successful post request

---
+++ test/trip.spec.js
@@ -0,0 +1,37 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import Trip from '../src/models/Trip';
+import server from '../src/server';
+import { mochaAsyncHelper } from './helpers';
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('Trip', () => {
+  describe('POST /trips', () => {
+    it('should create a trip successfully', mochaAsyncHelper(async () => {
+      const input = {
+        bus_id: 1,
+        origin: 'lagos',
+        destination: 'ibadan',
+        trip_date: new Date(2019, 11, 21).toISOString(),
+        fare: 2000.00,
+      };
+      const res = await chai.request(server)
+        .post('/api/v1/trips')
+        .send(input);
+      res.should.have.status(201);
+      res.body.should.have.property('id');
+      const record = await Trip.findOneById(res.body.id);
+      res.body.bus_id.should.be.eq(record.bus_id);
+      res.body.origin.should.be.eq(record.origin);
+      res.body.destination.should.be.eq(record.destination);
+      res.body.trip_date.should.be.eq(new Date(record.trip_date).toISOString());
+      res.body.fare.should.be.eq(record.fare);
+    }));
+    it('should only accept bus_id that exist in the system');
+    it('should not accept origin and destination that are the same');
+    it('should not accept trip_date that are in the past');
+    it('should only accept fare values greater than 0');
+  });
+});
